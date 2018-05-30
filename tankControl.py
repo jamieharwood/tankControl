57872100682891a4d8903b0cc2fc5cd600d1af67
Jamie <jamieharwood007@gmail.com>
2018-05-30 06:16:39 +0100

    Enhanced logging and Crash fixes

--- tankControl.py
+++ tankControl.py
@@ -7,8 +7,11 @@ Module: tankControl
 
 from machine import Pin
 from machine import RTC
+from machine import I2C
+from machine import PWM
 import network
 import machine
+import ssd1306
 import utime
 # import varibles as vars
 # import neopixel
@@ -21,14 +24,22 @@ from SensorRegistationClass import SensorRegistation
 from NeoPixelClass import NeoPixel
 from LogClass import Log
 
+i2c = I2C(-1, Pin(5), Pin(4))
+__oled = ssd1306.SSD1306_I2C(128, 32, i2c)
+__spinner = 0
+__statustest = ''
+__top = 0
 __log = Log()
 
 restHost = "http://192.168.86.240:5000"
 
+__sensorname = ''
+__deviceid = ''
+
 neoPin = 15  # D8
-pump = machine.PWM(machine.Pin(13), freq=500)  # D7
+pump = PWM(Pin(13), freq=500)  # D7
 hose = Pin(12, Pin.OUT)  # D6
-irrigation = Pin(5, Pin.OUT)  # D1
+irrigation = Pin(0, Pin.OUT)  # D3
 cooling = Pin(2, Pin.OUT)  # D4
 
 hose.on()
@@ -127,44 +138,70 @@ def getFullUrl(restFunction):
 
 
 def isstatechangedall():
-    returnvalue = 0
-    url = getFullUrl('getControlStates')
+    returnvalue = -1
+    timecount = 0
 
-    print(url)
+    while returnvalue == -1:
+        url = getFullUrl('getControlStates')
 
-    try:
-        response = urequests.get(url)
+        print(url)
 
         try:
-            sensorData = ujson.loads(response)
+            response = urequests.get(url)
 
-            returnvalue = sensorData
+            try:
+                sensorData = ujson.loads(response)
+
+                returnvalue = sensorData
+                # print('returnvalue=' + str(returnvalue))
+
+            except:
+                print("JSON error")
+
+            response.close()
+            break
         except:
-            print("JSON error")
+            print('isstatechangedall: Fail www connection attempt ' + str(timecount))
 
-        response.close()
-    except:
-        print('Fail www connect...')
+            timecount += 1
+
+            if timecount > 10:
+                import machine
+                machine.reset()
 
     return returnvalue
 
 
 def isstatechanged(state):
-    returnvalue = 0
-    # url = "http://192.168.86.240:5000/{0}/".replace('{0}', state)
-    url = getFullUrl(state)
+    returnvalue = -1
+    timecount = 0
 
-    print(url)
+    while returnvalue == -1:
+        # url = "http://192.168.86.240:5000/{0}/".replace('{0}', state)
+        url = getFullUrl(state)
 
-    try:
-        response = urequests.get(url)
+        print(url)
 
-        returnvalue = int(response.text.replace('\"', ''))
+        try:
+            response = urequests.get(url)
 
-        response.close()
-    except:
+            returnvalue = int(response.text.replace('\"', ''))
+            # print('returnvalue=' + str(returnvalue))
 
-        print('Fail www connect...')
+            response.close()
+
+            break
+        except:
+
+            print('isstatechanged: Fail www connection attempt ' + str(timecount))
+
+            timecount += 1
+
+            if timecount > 10:
+                import machine
+                machine.reset()
+
+            returnvalue = False
 
     return returnvalue
 
@@ -215,35 +252,92 @@ def testfornetwork():
         print('Waiting for IP')
 
 
-def main():
-    testfornetwork()
+def printoled(statustest, top=0):
+    global __statustest, __top
 
-    debug = False
-    sensorname = 'control'
+    __top = top
+    __statustest = statustest
+
+    __oled.fill(0)
+    __oled.text(__statustest, 0, __top)
+    __oled.show()
+
+
+def spinneroled():
+    global __deviceid, __sensorname,__spinner, __top
+
+    __oled.fill(0)
+
+    if __spinner == 0:
+        __oled.text('-', 120, 22)
+    elif __spinner == 1:
+        __oled.text('/', 120, 22)
+    elif __spinner == 2:
+        __oled.text('|', 120, 22)
+    elif __spinner == 3:
+        __oled.text('\\', 120, 22)
+
+    __oled.text(__statustest, 0, __top)
+    __oled.text(__sensorname, 0, 12)
+    __oled.text('dev: ' + __deviceid, 0, 22)
+    __oled.show()
+
+    __spinner += 1
+    if __spinner > 3:
+        __spinner = 0
+
+
+def settime(mytime):
+    timecount = 0
+
+    while not mytime.settime(1):
+        timecount += 1
+
+        if timecount > 10:
+            machine.reset()
+
+
+def main():
+    global __deviceid, __sensorname, __log
+
+    debug = True
+    __sensorname = 'control'
 
     if debug:
-        sensorname += '-debug'
+        __sensorname += '-debug'
 
-    deviceid = getdeviceid()
+    __deviceid = getdeviceid()
 
-    global __log
-    __log = Log(restHost, deviceid)
+    __log = Log(restHost, __deviceid)
+    printoled('tankControl v1')
 
-    mySensorRegistation = SensorRegistation(restHost, deviceid)
-    mySensorRegistation.register(sensorname, 'Hardware', 'JH')
+    testfornetwork()
+    __log.printl('Test for network.')
+    spinneroled()
+
+    __log.printl('startup: tankControl v1: ' + __sensorname)
+    spinneroled()
+
+    mySensorRegistation = SensorRegistation(restHost, __deviceid)
+    mySensorRegistation.register(__sensorname, 'Hardware', 'JH')
     __log.printl('registered: mySensorRegistation.register')
+    spinneroled()
 
-    myheartbeat = HeartBeat(restHost, deviceid)
+    myheartbeat = HeartBeat(restHost, __deviceid)
     myheartbeat.beat()
     __log.printl('registered: myheartbeat.beat')
+    spinneroled()
 
-    mytime = TimeTank(deviceid)
-    mytime.settime(1)
+    mytime = TimeTank(__deviceid, __log.printl)
+    settime(mytime)
+    # mytime.settime(1)
     __log.printl('registered: mytime.settime')
+    spinneroled()
 
     currTime = 0
     lastTime = 0
     lastMin = 0
+    lastSecond = -1
 
     iswet = 0
     tanklevel = 0
@@ -254,7 +348,8 @@ def main():
     getdisplay = 0
     getsunrise = 0
 
-    mytime.settime(1)
+    # settime(mytime)
+    # mytime.settime(1)
     rtc = RTC()
     sampletimes = [1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56]
     samplehours = [1, 6, 12, 18]
@@ -279,12 +374,18 @@ def main():
         timeNow = rtc.datetime()
         currHour = timeNow[4]
         currMinute = timeNow[5]
+        currSecond = timeNow[6]
+
+        if currSecond != lastSecond:
+            lastSecond = currSecond
+            spinneroled()
 
         if currMinute not in sampletimes and getdisplay == 0:
-
             getdisplay = 1
 
         if currMinute in sampletimes and getdisplay == 1:
+            getdisplay = 0
+
             __log.printl('sample times functions')
             isWet = isstatechanged('isWet')
             iswetdisplay(iswet)
@@ -292,34 +393,37 @@ def main():
             tanklevel = isstatechanged('isLevel')
             tankleveldisplay(tanklevel)
 
-            getdisplay = 0
-
         if currHour != 0 and currMinute != 1:
             getsunrise = 1
 
         if currHour == 0 and currMinute == 1 and getsunrise == 1:
-            __log.printl('Hourly functions')
-            mytime.settime(1)
-            __log.printl('registered: mytime.settime')
-
             getsunrise = 0
 
+            __log.printl('Hourly functions')
+            settime(mytime)
+            local = utime.localtime()
+            # mytime.settime(1)
+            __log.printl('registered: mytime.settime')
+
         if lastMin != currMinute:
+            lastMin = currMinute
+
             __log.printl('Minute functions')
             myheartbeat.beat()
             isSunrise = isstatechanged('isSunrise')
             isSunset = isstatechanged('isSunset')
 
-            lastMin = currMinute
-
         if currHour not in samplehours and gethour == 0:
             gethour = 1
 
         if currHour in samplehours and gethour == 1:
-            __log.printl('Sample hour functions')
             gethour = 0
+
+            __log.printl('Sample hour functions')
+
+            settime(mytime)
             local = utime.localtime()
-            mytime.settime(1)
+            # mytime.settime(1)
 
         switchsensorvalue = isstatechanged('isHose')
 
